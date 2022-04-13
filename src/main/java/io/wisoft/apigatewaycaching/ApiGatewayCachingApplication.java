a32a4dc026804b7d99b66438de042d4e074fbcc0
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-04-13 10:24:37 +0900

    initialize project

---
+++ src/main/java/io/wisoft/apigatewaycaching/ApiGatewayCachingApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.apigatewaycaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCachingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGatewayCachingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/wisoft/apigatewaycaching/ApiGatewayCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.apigatewaycaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayCachingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
