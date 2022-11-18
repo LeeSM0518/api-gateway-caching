e8f59cc962de36a4c7123fa34dfd9cc98ddc1032
Sangmin Lee <nalsm0518@si-analytics.ai>
2022-11-18 20:07:36 +0900

    filter update

--- src/main/java/io/wisoft/apigatewaycaching/filter/RequestGlobalQueryCachingFilter.java
+++ src/main/java/io/wisoft/apigatewaycaching/filter/RequestGlobalQueryCachingFilter.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -12,7 +13,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class RequestGlobalQueryCachingFilter implements GlobalFilter {
+public class RequestGlobalQueryCachingFilter implements GlobalFilter, Ordered {
 
   private final QueryCachingService queryCachingService;
 
@@ -20,6 +21,11 @@ public class RequestGlobalQueryCachingFilter implements GlobalFilter {
   public Mono<Void> filter(final ServerWebExchange exchange, final GatewayFilterChain chain) {
     log.info("filtered query caching");
     return queryCachingService.requestHandle(exchange, chain);
+  }
+
+  @Override
+  public int getOrder() {
+    return -3;
   }
 
 }
